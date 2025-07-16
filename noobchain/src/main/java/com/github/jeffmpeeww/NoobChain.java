ee0f0e98e6b42616239de1497cdcf796d12a7c04
JeffMpeeww <orefilemorule@gmail.com>
2025-07-16 16:30:50 +0200

    Added isChainValid() method to check the integrity of our blockchain
    The method loops through all the blocks in the chain and compares the hashes. It checks if the hash variable is equal to the calculated hash, and that the previous block's hash is equal to the previousHash variable.

--- noobchain/src/main/java/com/github/jeffmpeeww/NoobChain.java
+++ noobchain/src/main/java/com/github/jeffmpeeww/NoobChain.java
@@ -19,4 +19,30 @@ public class NoobChain {
         String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockChain);
         System.out.println(blockchainJson);
     }
+
+
+    public static Boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+
+        // Loop through blockchain to check hashes:
+        for (int i = 1; i < blockChain.size(); i++) {
+            currentBlock = blockChain.get(i);
+            previousBlock = blockChain.get(i-1);
+
+            // compare registered hash and calculated hash:
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                System.out.println("Current hashes not equal");
+                return false;
+            }
+
+            // compare previous registered and calculated hash
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("Previous hashes not equal");
+                return false;
+            }
+        }
+
+        return true;
+    }
 }

ac350bd7c7f6662a56a1e185b1b42df65e9dbaab
JeffMpeeww <orefilemorule@gmail.com>
2025-07-16 18:43:43 +0200

    Added the mineBlock() method and the nonce variable to be used in calculating the hash

--- noobchain/src/main/java/com/github/jeffmpeeww/Block.java
+++ noobchain/src/main/java/com/github/jeffmpeeww/Block.java
@@ -6,8 +6,9 @@ public class Block {
 
     public String hash;
     public String previousHash;
-    public String data; // our data will be a simple message
-    public long timeStamp; // as number of milliseconds
+    private String data; // our data will be a simple message
+    private long timeStamp; // as number of milliseconds
+    private int nonce;
 
     // Block Constructor
     public Block(String data, String previousHash) {
@@ -18,8 +19,17 @@ public class Block {
     }
 
     public String calculateHash() {
-        String calculatedHash = StringUtil.applySha256(previousHash + Long.toString(timeStamp) + data);
+        String calculatedHash = StringUtil.applySha256(previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + data);
         return calculatedHash;
     }
 
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0');  // Create a string with difficulty * "0"
+         while (!hash.substring(0, difficulty).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+         }
+
+         System.out.println("Block mined!!! : " + hash);
+    }
 }
